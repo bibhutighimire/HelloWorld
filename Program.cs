e7e1e97a0009e28eaa64b829b8a94689b6c5febf
bibhutighimire <ghimirebibhuti@gmail.com>
2020-06-23 22:58:12 -0600

    added program exit with enter key

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Bibu!");
+            Console.WriteLine("Press the ENTER key to close the program");
+            
+            String line = Console.ReadLine();
+
+if(line == "enter")
+{
+    System.Environment.Exit(0);
+}
+            
+        }
+    }
+}
